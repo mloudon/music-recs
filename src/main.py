1fc35c2b2a7582b574e8e754707b11ebc06c31b0
Melissa Loudon <melissa.loudon@gmail.com>
2015-05-31 10:56:48 -0400

    separate redis dbs for similarity matrices

--- src/main.py
+++ src/main.py
@@ -9,8 +9,10 @@ from settings import api_key
 
 
 base_url = 'http://ws.audioscrobbler.com/2.0/'
-r_store = redis.StrictRedis('localhost', 6379, decode_responses=True) # decode_responses returns keys in the default encoding
-max_retries = 3
+artist_tag_store = redis.StrictRedis('localhost', 6379, db=0, decode_responses=True) # decode_responses returns keys in the default encoding
+tag_sim_store = redis.StrictRedis('localhost', 6379, db=1, decode_responses=True)
+artist_sim_store = redis.StrictRedis('localhost', 6379, db=2, decode_responses=True)
+max_retries = 5
 
 def get_artists(count = 1000):
     try:
@@ -38,31 +40,33 @@ def get_top_tags(artist):
     
     return
 
-def create_data_store ():
-    clear_r_store()
-    artists = get_artists(1000)
+def lastfm_fetch ():
+    artists = get_artists(30)
     artist_names = [a['name'] for a in artists['artists']['artist']]
-    save_data(artist_names)
+    save_tags(artist_names)
     
     retries = 0
     incomplete = True
+    
     while retries <= max_retries and incomplete:
-        saved_artists = [k for k in r_store.scan_iter()]
+        saved_artists = [k for k in artist_tag_store.scan_iter()]
         missing_artists = list(set(artist_names) - set(saved_artists))
         
         if not missing_artists:
             incomplete = False
+            logging.info('No missing artists.')
         
         elif retries < max_retries:
             logging.warn('Missing tag data for artists: %s' % ','.join(missing_artists))
-            save_data(missing_artists)
-            retries = retries + 1
+            save_tags(missing_artists)
+            
+        retries = retries + 1
             
     if incomplete:
         logging.error('failed to get data for all artists')
         
 
-def save_data(artist_names):
+def save_tags(artist_names):
     if not artist_names:
         logging.error('no artist data')
         return
@@ -76,18 +80,20 @@ def save_data(artist_names):
             try:
                 tag_list = [t['name'] for t in tags['toptags']['tag']]
                 print ('artist: %s, tags: %s' % (artist,','.join(tag_list)))
-                r_store.rpush(artist, *tag_list)
-            except KeyError, e:
+                artist_tag_store.rpush(artist, *tag_list)
+            except (KeyError, TypeError) as e:
                 logging.error('No data saved for artist %s' % artist)
                 logging.error(e)
+                logging.error(tags)
         sleep(1)
         
-def print_r_store():
-    for key in r_store.scan_iter():
-        print ('artist: %s, tags: %s' % (key,','.join(r_store.lrange(key, 0, -1))))
+def artists_tags_print():
+    for key in artist_tag_store.scan_iter():
+        print ('artist: %s, tags: %s' % (key,','.join(artist_tag_store.lrange(key, 0, -1))))
         
-def clear_r_store():
-    r_store.flushdb()
+def clear_r_stores():
+    for r_store in [artist_tag_store,tag_sim_store,artist_sim_store]:
+        r_store.flushdb()
     
 def get_co_matrix():
     return
